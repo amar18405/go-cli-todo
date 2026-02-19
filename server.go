eddd035233c2a2b35d05f5166460b147174a8380
Amar Gupta <amargupta18405@gmail.com>
2026-02-19 19:44:10 +0530

    CRUD operations are done

--- server.go
+++ server.go
@@ -30,47 +30,96 @@ func startServer() {
 }
 
 func todoByIDHandler(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodPut {
-		http.Error(w, "Invalid Method Applied", http.StatusMethodNotAllowed)
-		return
-	}
 
 	parts := strings.Split(r.URL.Path, "/")
 
-	if len(parts) != 3 || parts[2] == "" {
-		http.Error(w, "Invalid URL ", http.StatusBadRequest)
+	// if len(parts) != 3 || parts[2] == "" {
+	// 	http.Error(w, "Invalid URL ", http.StatusBadRequest)
+	// 	return
+	// }
+
+	// id, err := strconv.Atoi(parts[2])
+	// if err != nil {
+	// 	http.Error(w, "Invalid IDProvided", http.StatusBadRequest)
+	// 	return
+	// }
+
+	// var req updateTodoRequest
+	// err = json.NewDecoder(r.Body).Decode(&req)
+	// if err != nil {
+	// 	http.Error(w, "Invalid Request Body", http.StatusBadRequest)
+	// 	return
+	// }
+
+	// for i, todo := range todos {
+	// 	if todo.ID == id {
+	// 		if req.Title != "" {
+	// 			todos[i].Title = req.Title
+	// 		}
+	// 		if req.Completed != nil {
+	// 			todos[i].Completed = *req.Completed
+	// 		}
+	// 		saveTodosToFile()
+
+	// 		w.Header().Set("Content-Type", "application/json")
+	// 		json.NewEncoder(w).Encode(todos[i])
+	// 		return
+	// 	}
+	// }
+	// http.Error(w, "Todo Not Found", http.StatusNotFound)
+
+	if len(parts) != 3 {
+		http.Error(w, "Invalid URL", http.StatusBadRequest)
 		return
 	}
 
 	id, err := strconv.Atoi(parts[2])
-	if err != nil {
-		http.Error(w, "Invalid IDProvided", http.StatusBadRequest)
-		return
+
+	if err != nil{
+		http.Error(w, " Invalid todo id ", http.StatusBadRequest)
 	}
 
-	var req updateTodoRequest
-	err = json.NewDecoder(r.Body).Decode(&req)
-	if err != nil {
-		http.Error(w, "Invalid Request Body", http.StatusBadRequest)
-		return
-	}
+	switch r.Method {
+	case http.MethodPost:
+		var req updateTodoRequest
 
-	for i, todo := range todos {
-		if todo.ID == id {
-			if req.Title != "" {
-				todos[i].Title = req.Title
-			}
-			if req.Completed != nil {
-				todos[i].Completed = *req.Completed
-			}
-			saveTodosToFile()
+		err := json.NewDecoder(r.Body).Decode(&req)
 
-			w.Header().Set("Content-Type", "application/json")
-			json.NewEncoder(w).Encode(todos[i])
-			return
+		if err != nil {
+			http.Error(w, "couldnt decode the request", http.StatusBadRequest)
 		}
+
+		for i, abcd := range todos{
+			if abcd.ID == id{
+				if req.Title != "" {
+					todos[i].Title = req.Title
+				}
+				if req.Completed != nil {
+					todos[i].Completed = *req.Completed
+				}
+				saveTodosToFile()
+				w.Header().Set("Content-Type", "application/json")
+				json.NewEncoder(w).Encode(todos[i])
+				return
+			}
+		} 	
+		http.Error(w,"No todo with given id was found", http.StatusBadRequest)
+		
+	case http.MethodDelete:
+		for i, abcd := range todos{
+			if abcd.ID == id{
+				todos = append(todos[:i], todos[i+1:]...)
+				saveTodosToFile()
+
+				w.WriteHeader(http.StatusNoContent)
+				return
+			}
+		}
+		http.Error(w,"given Id was not found", http.StatusBadRequest)
+	
+	default:
+		http.Error(w,"wrong method selected", http.StatusMethodNotAllowed)
 	}
-	http.Error(w, "Todo Not Found", http.StatusNotFound)
 }
 
 func todosHandler(w http.ResponseWriter, r *http.Request) {
